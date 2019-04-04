d1ac74899bc0ba7c343d47725f9b16a1daa775b0
Ryan W. Eaton <43385555+rweaton-ucd@users.noreply.github.com>
2019-04-04 15:32:25 -0700

    Update CalciumImagingBehaviorAnalysis.py
    Modified script into separate functions.  One a parser, the other a general comparator.

--- CalciumImagingBehaviorAnalysis.py
+++ CalciumImagingBehaviorAnalysis.py
@@ -10,92 +10,114 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-# Aquire path of the directory that contains this script.  This is so 
-# the routine can navigate back after changing directories if need be.  
-ScriptDir = os.getcwd()
-PathToFile = ''
-CellFluorTraces_Frame = pd.read_csv(PathToFile, header=0)
+def CinePlexCSV_parser(PathToFile):
+  
+  # Function takes a CSV file of present/absent events, converts changes
+  # of these states into timestamps, and returns each of the relevant
+  # lists of entry and exit events in a dictionary for subsequent
+  # processing.
+  
+  # Aquire path of the directory that contains this script.  This is so 
+  # the routine can navigate back after changing directories if need be.  
+  ScriptDir = os.getcwd()
+  #PathToFile = ''
+  CellFluorTraces_Frame = pd.read_csv(PathToFile, header=0)
 
-# Generate an index list for later referencing
-Indices = np.arange(0, CellFluorTraces_Frame['#'].size)
+  # Generate an index list for later referencing
+  Indices = np.arange(0, CellFluorTraces_Frame['#'].size)
 
-# Define a dictionary of key name pairs for referencing columns in the dataframe.
-# 
-# ### Description of nomenclature.  ###
-# Here "M6" and "M7" stand for Marker 6 (right hand) and Marker 7 (left hand)
-# respectively.  "T0", "T1" and "CT" stand for Target 0 (animal's right), 
-# Target 1 (animal's left) and Center Target respectively.  Tags "in" and "out"
-# refer to when the relevant marker is present or absent respectively within
-# the relevant target. Finally, "CumLen" stands for cumulative length in the
-# present target.
-ColumnNames = {'IndexNumber':'#',	
-               'Timestamp':'Timestamp',
-               'FrameNumber':'Frame_Number', 
-               'M6_xCoord':'X1.1_pix',
-               'M6_yCoord':'Y1.1_pix',
-               'M7_xCoord':'X2.1_pix',	
-               'M7_yCoord':'Y2.1_pix',
-               'M6T0_in':'EV1.1',
-               'M6T0_in_CumLen':'EV1.1_Track_Length_pix',
-               'M6T0_out':'EV1.2',
-               'M6T0_out_CumLen':'EV1.2_Track_Length_pix',
-               'M6T1_in':'EV1.3',
-               'M6T1_in_CumLen':'EV1.3_Track_Length_pix',
-               'M6T1_out':'EV1.4',
-               'M6T1_out_CumLen':'EV1.4_Track_Length_pix',
-               'M6CT_in':'EV1.5',
-               'M6CT_in_CumLen':'EV1.5_Track_Length_pix',
-               'M6CT_out':'EV1.6',
-               'M6CT_out_CumLen':'EV1.6_Track_Length_pix',
-               'M7T0_in':'EV1.7',
-               'M7T0_in_CumLen':'EV1.7_Track_Length_pix',
-               'M7T0_out':'EV1.8',
-               'M7T0_out_CumLen':'EV1.8_Track_Length_pix',
-               'M7T1_in':'EV1.9',
-               'M7T1_in_CumLen':'EV1.9_Track_Length_pix',
-               'M7T1_out':'EV1.10',
-               'M7T1_out_CumLen':'EV1.10_Track_Length_pix',
-               'M7CT_in':'EV1.11',
-               'M7CT_in_CumLen':'EV1.11_Track_Length_pix',
-               'M7CT_out':'EV1.12',
-               'M7CT_out_CumLen':'EV1.12_Track_Length_pix'}
+  # Define a dictionary of key name pairs for referencing columns in the dataframe.
+  # 
+  # ### Description of nomenclature.  ###
+  # Here "M6" and "M7" stand for Marker 6 (right hand) and Marker 7 (left hand)
+  # respectively.  "T0", "T1" and "CT" stand for Target 0 (animal's right), 
+  # Target 1 (animal's left) and Center Target respectively.  Tags "in" and "out"
+  # refer to when the relevant marker is present or absent respectively within
+  # the relevant target. Finally, "CumLen" stands for cumulative length in the
+  # present target.
+  ColumnNames = {'IndexNumber':'#',	
+                 'Timestamp':'Timestamp',
+                 'FrameNumber':'Frame_Number', 
+                 'M6_xCoord':'X1.1_pix',
+                 'M6_yCoord':'Y1.1_pix',
+                 'M7_xCoord':'X2.1_pix',	
+                 'M7_yCoord':'Y2.1_pix',
+                 'M6T0_in':'EV1.1',
+                 'M6T0_in_CumLen':'EV1.1_Track_Length_pix',
+                 'M6T0_out':'EV1.2',
+                 'M6T0_out_CumLen':'EV1.2_Track_Length_pix',
+                 'M6T1_in':'EV1.3',
+                 'M6T1_in_CumLen':'EV1.3_Track_Length_pix',
+                 'M6T1_out':'EV1.4',
+                 'M6T1_out_CumLen':'EV1.4_Track_Length_pix',
+                 'M6CT_in':'EV1.5',
+                 'M6CT_in_CumLen':'EV1.5_Track_Length_pix',
+                 'M6CT_out':'EV1.6',
+                 'M6CT_out_CumLen':'EV1.6_Track_Length_pix',
+                 'M7T0_in':'EV1.7',
+                 'M7T0_in_CumLen':'EV1.7_Track_Length_pix',
+                 'M7T0_out':'EV1.8',
+                 'M7T0_out_CumLen':'EV1.8_Track_Length_pix',
+                 'M7T1_in':'EV1.9',
+                 'M7T1_in_CumLen':'EV1.9_Track_Length_pix',
+                 'M7T1_out':'EV1.10',
+                 'M7T1_out_CumLen':'EV1.10_Track_Length_pix',
+                 'M7CT_in':'EV1.11',
+                 'M7CT_in_CumLen':'EV1.11_Track_Length_pix',
+                 'M7CT_out':'EV1.12',
+                 'M7CT_out_CumLen':'EV1.12_Track_Length_pix'}
 
-# Extract Marker 6 T0 entry events
-Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M6T0_in']])== 1))
-M6T0_Entry_ind = Indices[Filt]
-M6T0_Entry_ts = CellFluorTraces_Frame[ColumnNames['M6T0_in']][M6T0_Entry_ind]
+  # Extract Marker 6 T0 entry events
+  Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M6T0_in']])== 1))
+  M6T0_Entry_ind = Indices[Filt]
+  M6T0_Entry_ts = CellFluorTraces_Frame[ColumnNames['M6T0_in']][M6T0_Entry_ind]
 
-# Extract Marker 6 T1 entry events
-Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M6T1_in']])== 1))
-M6T1_Entry_ind = Indices[Filt]
-M6T1_Entry_ts = CellFluorTraces_Frame[ColumnNames['M6T1_in']][M6T1_Entry_ind]
+  # Extract Marker 6 T1 entry events
+  Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M6T1_in']])== 1))
+  M6T1_Entry_ind = Indices[Filt]
+  M6T1_Entry_ts = CellFluorTraces_Frame[ColumnNames['M6T1_in']][M6T1_Entry_ind]
 
-# Extract Marker 6 CT exit events
-Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M6CT_out']])== 1))
-M6CT_Exit_ind = Indices[Filt]
-M6CT_Exit_ts = CellFluorTraces_Frame[ColumnNames['M6CT_out']][M6CT_Exit_ind]
+  # Extract Marker 6 CT exit events
+  Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M6CT_out']])== 1))
+  M6CT_Exit_ind = Indices[Filt]
+  M6CT_Exit_ts = CellFluorTraces_Frame[ColumnNames['M6CT_out']][M6CT_Exit_ind]
 
-# Extract Marker 7 T0 entry events
-Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M7T0_in']])== 1))
-M7T0_Entry_ind = Indices[Filt]
-M7T0_Entry_ts = CellFluorTraces_Frame[ColumnNames['M7T0_in']][M7T0_Entry_ind]
+  # Extract Marker 7 T0 entry events
+  Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M7T0_in']])== 1))
+  M7T0_Entry_ind = Indices[Filt]
+  M7T0_Entry_ts = CellFluorTraces_Frame[ColumnNames['M7T0_in']][M7T0_Entry_ind]
 
-# Extract Marker 7 T1 entry events
-Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M7T1_in']])== 1))
-M7T1_Entry_ind = Indices[Filt]
-M7T1_Entry_ts = CellFluorTraces_Frame[ColumnNames['M7T1_in']][M6T1_Entry_ind]
+  # Extract Marker 7 T1 entry events
+  Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M7T1_in']])== 1))
+  M7T1_Entry_ind = Indices[Filt]
+  M7T1_Entry_ts = CellFluorTraces_Frame[ColumnNames['M7T1_in']][M6T1_Entry_ind]
 
-# Extract Marker 7 CT exit events
-Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M7CT_out']])== 1))
-M7CT_Exit_ind = Indices[Filt]
-M7CT_Exit_ts = CellFluorTraces_Frame[ColumnNames['M7CT_out']][M7CT_Exit_ind]
-
-# Write a function that tests if each entry t_2 in tlist2 is within the range 
-# [t_1 + tol_lo, t_1 + tol_hi) for each entry in tlist1.  Output is an object array of size 
-# tlist1.size having each field contain a list of indices that point to all entries
-# in tlist2 that lie within the range. Note, for t + tol_lo to precede t, tol_lo must
-# be negative-valued.
+  # Extract Marker 7 CT exit events
+  Filt = np.hstack((False, np.diff(CellFluorTraces_Frame[ColumnNames['M7CT_out']])== 1))
+  M7CT_Exit_ind = Indices[Filt]
+  M7CT_Exit_ts = CellFluorTraces_Frame[ColumnNames['M7CT_out']][M7CT_Exit_ind]
+  
+  return {
+          'M6T0_Entry_ind' : M6T0_Entry_ind,
+          'M6T0_Entry_ts': M6T0_Entry_ts,
+          'M6T1_Entry_ind' : M6T1_Entry_ind,
+          'M6T1_Entry_ts' : M6T1_Entry_ts,
+          'M6CT_Exit_ind' : M6CT_Exit_ind,
+          'M6CT_Exit_ts' : M6CT_Exit_ts,
+          'M7T0_Entry_ind' : M7T0_Entry_ind,
+          'M7T0_Entry_ts' : M7T0_Entry_ts,
+          'M7T1_Entry_ind' : M7T1_Entry_ind,
+          'M7T1_Entry_ts' : M7T1_Entry_ts,
+          'M7CT_Exit_ind' : M7CT_Exit_ind,
+          'M7CT_Exit_ts' : M7CT_Exit_ts
+         }
 def EventComparator(tlist1, tlist2, tol_window):
+  
+  # This tests if each entry t_2 in tlist2 is within the range [t_1 + tol_lo, t_1 + tol_hi) 
+  # for each entry in tlist1.  Output is an object array of size 
+  # tlist1.size having each field contain a list of indices that point to all entries
+  # in tlist2 that lie within the range. Note, for t + tol_lo to precede t, tol_lo must
+  # be negative-valued.
   
   # Extract low tolerance and high tolerance boundaries from the tol_window tuple.
   (tol_lo, tol_hi) = tol_window
